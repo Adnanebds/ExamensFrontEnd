bec46f367d24b381460ed42edcc59f1e2fffb36a
SimonShahan <simonshahan@icloud.com>
2023-01-23 11:40:50 +0100

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -52,10 +52,6 @@ const Login = () => {
   // if there's no user, show the login form
   return (
     <div>
-    
-
-
-<div>
 <section class="bg-gray-50 dark:bg-gray-900">
   <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
@@ -96,8 +92,6 @@ const Login = () => {
       </div>
   </div>
 </section>
-</div>
-
 </div>
   );
 };
