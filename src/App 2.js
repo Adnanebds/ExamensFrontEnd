f280241c51274f36ce64e35c6579df1d59f7d581
SimonShahan <simonshahan@icloud.com>
2023-01-24 12:15:30 +0100

    magazijn

---
+++ src/App 2.js
@@ -0,0 +1,42 @@
+import './App.css';
+import {
+  BrowserRouter, 
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Home from "./components/Home";
+import Login from './components/Login';
+import Registration from './components/Registration';
+import Magazijn from './components/Magazijn';
+import Contact from './components/Contact';
+import Forgot from './components/ForgotPassword'
+import useToken from './scripts/useToken';
+
+
+
+
+function App() {
+  
+  const { token, setToken } = useToken();
+  
+    if(!token || token === undefined) {
+      return <Login setToken={setToken} />
+      }
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/registreren" element={<Registration/>}/>
+        <Route path="/magazijn" element={<Magazijn/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/vergeten" element={<Forgot/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+
+}
+
+
+export default App;
